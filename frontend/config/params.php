bfd4f024829f4458e354d48bccc923730583562b
yuhaiqun <yuhaiqunyuhaiqun@126.com>
2017-02-23 16:51:28 +0800

    获取微信的openid

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -3,6 +3,8 @@ return [
     'adminEmail' => '13661997454@139.com',
     'redisCount' => 'BLOG_READCOUNT_',
     'wechat' =>[
-        'token' => 'your token',
+        'token' => 'weixin',
+        'appid' => 'wxa18038aad42509ad',
+        'appsecret' => 'e3d545696d15a4c870a4aa32cbf59ac4',
     ],
 ];
--- frontend/controllers/WechatController.php
+++ frontend/controllers/WechatController.php
@@ -105,4 +105,58 @@ class WechatController extends Controller
             return false;
         }
     }
+    //用户授权接口：获取access_token、openId等；获取并保存用户资料到数据库
+    public function actionAccesstoken()
+    {
+        $code = $_GET["code"];
+        $state = $_GET["state"];
+        $appid = Yii::$app->params['wechat']['appid'];
+        $appsecret = Yii::$app->params['wechat']['appsecret'];
+        $request_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$appid.'&secret='.$appsecret.'&code='.$code.'&grant_type=authorization_code';
+        //初始化一个curl会话
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $request_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $result = $this->response($result);
+        //获取token和openid成功，数据解析
+        $access_token = $result['access_token'];
+        $refresh_token = $result['refresh_token'];
+        $openid = $result['openid'];
+        //请求微信接口，获取用户信息
+        $userInfo = $this->getUserInfo($access_token,$openid);
+        $user_check = WechatUser::find()->where(['openid'=>$openid])->one();
+        if ($user_check) {
+            //更新用户资料
+            $user_check->nickname = $userInfo['nickname'];
+            $user_check->sex = $userInfo['sex'];
+            $user_check->headimgurl = $userInfo['headimgurl'];
+            $user_check->country = $userInfo['country'];
+            $user_check->province = $userInfo['province'];
+            $user_check->city = $userInfo['city'];
+            $user_check->access_token = $access_token;
+            $user_check->refresh_token = $refresh_token;
+            $user_check->update();
+        } else {
+            //保存用户资料
+            $user = new WechatUser();
+            $user->nickname = $userInfo['nickname'];
+            $user->sex = $userInfo['sex'];
+            $user->headimgurl = $userInfo['headimgurl'];
+            $user->country = $userInfo['country'];
+            $user->province = $userInfo['province'];
+            $user->city = $userInfo['city'];
+            $user->access_token = $access_token;
+            $user->refresh_token = $refresh_token;
+            $user->openid = $openid;
+            $user->save();
+        }
+        //前端网页的重定向
+        if ($openid) {
+            return $this->redirect($state.$openid);
+        } else {
+            return $this->redirect($state);
+        }
+    }
 }
